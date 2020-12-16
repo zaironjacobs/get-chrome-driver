84019ce3ffd59de6ef1c180ef46dbf0745f33bba
Zairon Jacobs <zaijacobs@gmail.com>
2020-12-16 16:12:52 +0100

    version

--- get_chromedriver/__init__.py
+++ get_chromedriver/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.1.11'
+__version__ = '1.1.12'
 
 from .get_driver import GetChromeDriver
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 name = 'get-chromedriver'
-version = '1.1.11'
+version = '1.1.12'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
