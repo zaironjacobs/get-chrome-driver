b63680e02c4d4c4a02de4f418ba6b38165e3e6ce
Zairon Jacobs <zaijacobs@gmail.com>
2021-04-26 21:43:11 +0200

    css selector update

--- get_chrome_driver/__init__.py
+++ get_chrome_driver/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.1.42'
+__version__ = '1.2'
 
 from .get_driver import GetChromeDriver
--- get_chrome_driver/constants.py
+++ get_chrome_driver/constants.py
@@ -6,7 +6,7 @@ ZIP_TYPE = '.zip'
 CHROMEDRIVER = 'chromedriver'
 WEBDRIVER = 'webdriver'
 
-CSS_VERSIONS_SELECTOR_UL = '.sites-layout-tile > div:nth-child(1) > div:nth-child(8) > ul:nth-child(1)'
+CSS_VERSIONS_SELECTOR_UL = 'ul.n8H08c:nth-child(5)'
 
 LATEST_STABLE_VERSION_STR = 'Latest stable release'
 LATEST_BETA_VERSION_STR = 'Latest beta release'
